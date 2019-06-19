add21b300ada3c4b4a1bdf5c166175fad20f43e9
Shyam Sunder <shyam.udupa@gmail.com>
2019-06-19 16:31:54 +0530

    yaaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, 'D:\\shyam\\Documents\\public\\plutoPy')
+sys.path.insert(0, '..\\..\\plutoPy')
 
 # -- Project information -----------------------------------------------------
 
