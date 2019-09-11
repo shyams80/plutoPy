cedc1ffd21493bd91be69bcb1f0690594e4cb332
Shyam Sunder <shyam.udupa@gmail.com>
2019-09-11 18:21:05 +0530

    config path

--- plutoPy/Config.py
+++ plutoPy/Config.py
@@ -1,3 +1,7 @@
 import configparser
+import os
+
+path = os.path.join(os.environ.get('PLUTO_PY_CONFIG_PATH', ''), 'config.ini')
+
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(path)
--- plutoPy/model/RoutingSession.py
+++ plutoPy/model/RoutingSession.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 import urllib.parse
+from pydoc import locate
 
 from .Db import StockViz, StockVizUs, StockVizUs2, StockVizDyn, StockVizBeka
 
@@ -10,28 +11,18 @@ import plutoDbPy
 dbNames = config['DEFAULT']['DB_NAMES'].split(",")
 dbNames = [dn.strip() for dn in dbNames]
 
-qpass = urllib.parse.quote_plus(config['DEFAULT']['REDIS_PASSWORD'])
-redisSever = config['DEFAULT']['REDIS_SERVER']
-
 engines = {}
 for dbName in dbNames:
-    conTemplate = config["CONNECTIONS"][dbName].replace("{XXX}", f":{qpass}@{redisSever}")
-    engines[dbName] = create_engine(conTemplate, module = plutoDbPy.dbapi, echo=False)
+    engines[dbName] = create_engine(config["CONNECTIONS"][dbName], module = plutoDbPy.dbapi, echo=False)
       
 
 class RoutingSession(Session):
     def get_bind(self, mapper=None, clause=None):
-        if mapper and issubclass(mapper.class_, StockViz):
-            return engines['StockViz']
-        elif mapper and issubclass(mapper.class_, StockVizUs):
-            return engines['StockVizUs']
-        elif mapper and issubclass(mapper.class_, StockVizUs2):
-            return engines['StockVizUs2']
-        elif mapper and issubclass(mapper.class_, StockVizDyn):
-            return engines['StockVizDyn']
-        elif mapper and issubclass(mapper.class_, StockVizBeka):
-            return engines['StockVizBeka']
-        elif self._flushing:
+        for dbName in dbNames:
+            if mapper and issubclass(mapper.class_, locate("plutoPy.model.Db." + dbName)):
+                return engines[dbName]
+            
+        if self._flushing:
             raise Exception("Unknown database!")
         
 Session = sessionmaker(class_ = RoutingSession)
